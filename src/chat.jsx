a87d1c9aab0d3308c8cef7dd0ff8ce99aa772118
tempest <tempesttempest66o@gmail.com>
2024-05-17 13:43:16 -0300

    cambio2

--- src/chat.jsx
+++ src/chat.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:4000'); 
+const socket = io(process.env.REACT_APP_BACKEND_URL);
 
 
 
